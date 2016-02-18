06b9125cc9f6eb696161636192980d1228438b56
aka LYQ / Virose <mouzugaming@outlook.de>
2016-02-18 10:20:34 +0100

    including RaidOfficer plugin

---
+++ Plugins/RaidOfficer.lua
@@ -0,0 +1,18 @@
+--[[
+    by LYQ(Virose / MOUZU)
+    https://github.com/MOUZU/BigWigs
+    
+    This small plugin simply allows to move players in the raidgroup
+    from one subgroup to another, if the player using this version
+    is a RaidAssistant.
+    Previously this was possible in blizzards API but not implemented.
+--]]
+
+--[[
+    TODO:
+        functions to be used
+        SetRaidSubgroup(playerIndex 1-40/MAX_RAID_MEMBERS, subgroupIndex)
+        SwapRaidSubgroup(player1Index, player2Index)
+        
+        (IsRaidLeader() or IsRaidOfficer())
+]]
