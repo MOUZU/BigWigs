8f148b8be3decc39325188e59b6a8469189ce935
aka LYQ / Virose <mouzugaming@outlook.de>
2016-02-18 09:39:08 +0100

    GazeIcon shall be displayed on Incoming Gaze as well

--- Raids/ZG/Mandokir.lua
+++ Raids/ZG/Mandokir.lua
@@ -226,6 +226,7 @@ function BigWigsMandokir:BigWigs_RecvSync(sync, rest, nick)
 	elseif sync == "MandokirGazeCast" and self.db.profile.gaze then
 		self:TriggerEvent("BigWigs_StartBar", self, L["gazecast"], 2, "Interface\\Icons\\Spell_Shadow_Charm")
         self:TriggerEvent("BigWigs_StopBar", self, "Next Gaze")
+        self:TriggerEvent("BigWigs_ShowIcon", "Interface\\Icons\\Spell_Shadow_Charm", 2)
 	elseif sync == "MandokirGazeAfflict" and self.db.profile.gaze then
 		self:TriggerEvent("BigWigs_StartBar", self, string.format(L["gazewatchedbar"], rest), 5, "Interface\\Icons\\Spell_Shadow_Charm", true, "Black")
 	elseif sync == "MandokirGazeEnd" then
