5f33dc7ef005f83328783f7dd2772a329b772550
aka LYQ / Virose <mouzugaming@outlook.de>
2016-02-24 21:19:09 +0100

    minor enhancement

--- Plugins/ReadyCheck.lua
+++ Plugins/ReadyCheck.lua
@@ -14,7 +14,11 @@ local c = {
         -- this list is resetting every finished ready check
         -- it keeps track of the results from the ongoing ready check
         -- [player] = response,
+        --      -1  =   ??
+        --      0   =   not ready
+        --      1   =   ready
     },
+    statusListIndex = {},
     initialized     = false,    -- init process(overwrite things)
     
     ackList         = {
@@ -25,6 +29,7 @@ local c = {
         -- this shall cache who is not using a compatible version
     },
 }
+local f = CreateFrame("Frame")
 
 ----------------------------
 --      Localization      --
@@ -43,7 +48,9 @@ BigWigsReadyCheck = BigWigs:NewModule("ReadyCheck")
 
 function BigWigsReadyCheck:OnEnable()
     self:RegisterEvent("BigWigs_RecvSync")
-    self:ScheduleEvent("ReadyCheckSetup", BigWigsReadyCheck.SetupFrames, 2) -- does this fix the lua error?
+    self:RegisterEvent("RAID_ROSTER_UPDATE", "UpdateButton")
+    self:RegisterEvent("PARTY_CONVERTED_TO_RAID", "UpdateButton")
+    self:ScheduleEvent("ReadyCheckSetup", BigWigsReadyCheck.SetupFrames, 2)
 end
 
 ------------------------------
@@ -58,38 +65,72 @@ function BigWigsReadyCheck:BigWigs_RecvSync(sync, rest, nick)
         - a raidmember does have it and confirms it, he will acknowledge and confirm
         - a raidmember does have it and denies it(he is not ready but not afk), he will acknowledge and deny
     --]]
-    if sync == "ReadyCheckRequest" then
+    if sync == "ReadyCheckRequest" and nick ~= UnitName("player") then
         -- a RaidOfficer started a ReadyCheck, we acknowledge his request
         self:TriggerEvent("BigWigs_SendSync", "ReadyCheckAcknowledge")
-        ReadyCheckFrame:Show()
         
         -- we clear the result table
         for unit=1, GetNumRaidMembers() do
             local name = GetRaidRosterInfo(unit)
-            c.statusList[name] = nil
+            c.statusList[name] = -1
         end
         -- we clear the ack table because we want to know only the results of this current raid setup
         for i=1, table.getn(c.ackList) do
             c.ackList[i] = nil
         end
         
+        -- setting up the popup window
+        for i=1, MAX_RAID_MEMBERS do
+            local name = GetRaidRosterInfo(i)
+            if name and name == rest then
+                SetPortraitTexture(ReadyCheckPortrait, "raid"..i)
+                break
+            end
+        end
+        ReadyCheckFrameText:SetText(format(READY_CHECK_MESSAGE, rest))        
+        ReadyCheckFrame:Show()
+        c.lastReadyCheck = GetTime()
+        PlaySound("ReadyCheck")
+        
+        
+        
+        
         -- TODO: setup OnUpdate to hide the frame after 29s again
         --          if the time runs out and the player did not respond, make a message that the player missed a readycheck
     elseif sync == "ReadyCheckAcknowledge" then
         -- another raidmember acknowledged the request
         tinsert(c.ackList, nick)
-        
+        if not c.statusList[nick] then
+            c.statusList[nick] = -1
+        end
         -- TODO: display a question mark on his raidframe
     elseif sync == "ReadyCheckConfirm" then
-        -- a raidmember confirmed the request
-        c.statusList[nick] = true
         
-        -- TODO: display a green check sign on his raidframe (hide the question mark)
-    elseif sync == "ReadyCheckDeny" then
-        -- a raidmember denied the request
-        c.statusList[nick] = false
+        if rest == "Y" then
+            c.statusList[nick] = 1
+            -- TODO: display a green check sign on his raidframe (hide the question mark)
+        elseif rest == "N" then
+            c.statusList[nick] = 0
+            -- TODO: display a red cross on his raidframe (hide the question mark)
+        end
         
-        -- TODO: display a red cross on his raidframe (hide the question mark)
+        -- 8seconds is the minimum time for a readycheck. I expect the ackList to be full by then
+        if (c.lastReadyCheck + 8) < GetTime() and table.getn(c.statusListIndex) == table.getn(c.ackList) then
+            -- check who is not using the addon
+            for i=1, GetNumRaidMembers() do
+                if not c.statusList[UnitName("raid"..i)] then
+                    for j=1, table.getn(c.withoutAddOn) do
+                        if c.withoutAddOn[j] == UnitName("raid"..i) then
+                            -- TODO btw if case is probably not what I want
+                        end
+                    end
+                end
+            end
+            
+            -- check the results of the ones with the addon
+            
+        end
+        
     end
 end
 
@@ -97,7 +138,6 @@ function BigWigsReadyCheck:SetupFrames()
     -- I'm overwriting the related Button scripts to use my sync
     if not c.initialized then
         RaidFrameReadyCheckButton:SetScript('OnClick', function()
-                --DoReadyCheck()    I'll not need that anymore
                 
                 -- init onupdate and possibly enable readycheck for raid assistants too, need to make the button for them visible beforehand too
                 -- OnUpdate for - disabling the button for 30s while the current ReadyCheck is ongoing. and analyzing the results of it.
@@ -105,19 +145,38 @@ function BigWigsReadyCheck:SetupFrames()
                 -- how long should the results be visible after the time is up or all ack members responded? 5s? 10s?
                 -- TODO: check for compatibility of Grid, sRaidFrames and LunaRaidFrames
                 -- TO RETHINK: option to display AFK players in raidchat or maybe players without the addon?, these options should only be for RL
+                
+                BigWigsReadyCheck:TriggerEvent("BigWigs_SendSync", "ReadyCheckRequest " .. UnitName("player"))
+                c.lastReadyCheck = GetTime()
             end)
         
         ReadyCheckFrameYesButton:SetScript('OnClick', function()
-                --ConfirmReadyCheck(1)  I'll not need that anymore
                 ReadyCheckFrame:Hide()
-                BigWigsReadyCheck:TriggerEvent("BigWigs_SendSync", "ReadyCheckConfirm")
+                BigWigsReadyCheck:TriggerEvent("BigWigs_SendSync", "ReadyCheckConfirm Y")
             end)
         ReadyCheckFrameNoButton:SetScript('OnClick', function()
-                --ConfirmReadyCheck()   I'll not need that anymore
                 ReadyCheckFrame:Hide()
-                BigWigsReadyCheck:TriggerEvent("BigWigs_SendSync", "ReadyCheckDeny")
+                BigWigsReadyCheck:TriggerEvent("BigWigs_SendSync", "ReadyCheckConfirm N")
+            end)
+        -- the following is the popup window update, it was only used to alert the player if he missed a check - this does not suffice in our case
+        ReadyCheckFrame:SetScript('OnUpdate', nil)
+        f:SetScript('OnUpdate', function()
+                if c.lastReadyCheck < GetTime() then
+                    -- TODO
+                end
             end)
         
+        BigWigsReadyCheck:UpdateButton()
+        
         c.initialized = true
+    end
+end
+
+function BigWigsReadyCheck:UpdateButton()
+    -- by default this is hidden and on this update it would hide the button again
+    if GetNumRaidMembers() > 0 and IsRaidOfficer() then
+        RaidFrameReadyCheckButton:Show()
+    else
+        RaidFrameReadyCheckButton:Hide()
     end
 end
