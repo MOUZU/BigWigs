0149d4fe53c1e88fb68f4191b2a6b079dd63e685
aka LYQ / Virose <mouzugaming@outlook.de>
2016-03-02 16:31:34 +0100

    finished RaidOfficer plugin

--- Plugins/RaidOfficer.lua
+++ Plugins/RaidOfficer.lua
@@ -8,11 +8,72 @@
     Previously this was possible in blizzards API but not implemented.
 --]]
 
---[[
-    TODO:
-        functions to be used
-        SetRaidSubgroup(playerIndex 1-40/MAX_RAID_MEMBERS, subgroupIndex)
-        SwapRaidSubgroup(player1Index, player2Index)
-        
-        (IsRaidLeader() or IsRaidOfficer())
-]]
+----------------------------------
+--      Module Declaration      --
+----------------------------------
+
+BigWigsRaidOfficer = BigWigs:NewModule("RaidOfficer")
+
+------------------------------
+--      Initialization      --
+------------------------------
+
+function BigWigsRaidOfficer:OnEnable()
+    self:ScheduleEvent(self.SetupFrames,2)
+end
+
+------------------------------
+--      Event Handlers      --
+------------------------------
+
+function BigWigs_RaidGroupButton_OnDragStart()
+	if ( not IsRaidLeader() and not IsRaidOfficer() ) then
+		return;
+	end
+	local cursorX, cursorY = GetCursorPosition();
+	this:ClearAllPoints();
+	this:SetPoint("CENTER", nil, "BOTTOMLEFT", cursorX*GetScreenWidthScale(), cursorY*GetScreenHeightScale());
+	this:StartMoving();
+	MOVING_RAID_MEMBER = this;
+	SetRaidRosterSelection(this.id);
+end
+
+function BigWigs_RaidGroupButton_OnDragStop(raidButton)
+	if ( not IsRaidLeader() and not IsRaidOfficer() ) then
+		return;
+	end
+	if ( not raidButton ) then
+		raidButton = this;
+	end
+	
+	raidButton:StopMovingOrSizing();
+	MOVING_RAID_MEMBER = nil;
+	if ( TARGET_RAID_SLOT and TARGET_RAID_SLOT:GetParent():GetID() ~= raidButton.subgroup ) then
+		if (TARGET_RAID_SLOT.button) then
+			local button = getglobal(TARGET_RAID_SLOT.button);
+			--button:SetPoint("TOPLEFT", this, "TOPLEFT", 0, 0);
+			SwapRaidSubgroup(raidButton:GetID(), button:GetID());
+		else
+			local slot = TARGET_RAID_SLOT:GetParent():GetName().."Slot"..TARGET_RAID_SLOT:GetParent().nextIndex;
+			raidButton:ClearAllPoints();
+			raidButton:SetPoint("TOPLEFT", slot, "TOPLEFT", 0, 0);
+			TARGET_RAID_SLOT:UnlockHighlight();
+			SetRaidSubgroup(raidButton:GetID(), TARGET_RAID_SLOT:GetParent():GetID());
+		end
+	else
+		if ( TARGET_RAID_SLOT ) then
+			TARGET_RAID_SLOT:UnlockHighlight();
+		end
+		raidButton:ClearAllPoints();
+		raidButton:SetPoint("TOPLEFT", raidButton.slot, "TOPLEFT", 0, 0);
+	end
+end
+
+function BigWigsRaidOfficer:SetupFrames()
+    -- backup the old ones just in case
+    ORIGINAL_RaidGroupButton_OnDragStart = RaidGroupButton_OnDragStart
+    ORIGINAL_RaidGroupButton_OnDragStop = RaidGroupButton_OnDragStop
+    -- overwrite them
+    RaidGroupButton_OnDragStart = BigWigs_RaidGroupButton_OnDragStart
+    RaidGroupButton_OnDragStop = BigWigs_RaidGroupButton_OnDragStop
+end
